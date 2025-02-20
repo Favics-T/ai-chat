0be0c3992b8910a992a81e25dbf728ef4bafc0d6
unknown <taiwokolapo533@gmail.com>
2025-02-20 19:00:16 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import {HashRouter as Router,Routes,Route,Outlet} from 'react-router-dom';
 import Home from './page/Home';
 import Chats from './page/Chats';
 
-const Layout = ()=>{
- return <div>
-    
-    <Outlet />
-  </div>
-}
+useEffect(() => {
+  const originMeta = document.createElement("meta");
+  originMeta.httpEquiv = "origin-trial";
+  originMeta.content = import.meta.env.VITE_SUMMARIZER_TOKEN;  // Fetch from .env file
+
+  document.head.appendChild(originMeta);
+  console.log("Origin Trial Token Added:", originMeta.content);
+
+  return () => {
+      document.head.removeChild(originMeta); // Cleanup when component unmounts
+  };
+}, []);
+
 
 const App = () => {
   return (
